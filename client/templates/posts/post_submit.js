39b2c8940fa1d5a98270f79e203dca779c72e991
JoaquinSiabra <joaquin.siabra@gmail.com>
2015-09-22 10:38:28 +0000

    compensacion de latencia

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,8 +12,8 @@ Template.postSubmit.events({
       if (error) return alert(error.reason);
       if (result.postExists) alert('This link has already been posted');
 
-      Router.go('postPage', {_id: result._id});
-    });
 
+    });
+  Router.go('postsList');
   }
 });
